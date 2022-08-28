e68440d9a1a5609fe8eb9972f93e5b790dc1cbc1
Marcelle Vargas da Silva <marcelle.vargas@arctouch.com>
2022-08-28 15:26:38 -0300

    create custom header

--- header.php
+++ header.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/style.css' ?>">
   </head>
   <body>
+    <?php if((get_header_image())) : ?>
       <header class="header-site-book-store">
+          <img class="header-site-book-store__background" src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
           <div class="header-site-book-store__logo">
               <?php the_custom_logo(); ?>
           </div>
@@ -24,4 +26,6 @@
               </nav>
           </div>
       </header>
+<?php endif; ?>
+
   
